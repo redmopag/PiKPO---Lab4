f3cb49b0347ed5105bd0397ca53d9b510540890f
redmopag <redmopag@gmail.com>
2023-04-16 00:50:54 +0500

    Some changes
    Changed dataprocessorservice. Added new method for working with database

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args)
     {
         // Без указания полного пути, программа будет читать файл из своей корневой папки
-        DataProcessorService service = new DataProcessorService("Прирост населения.csv");
+        DataProcessorService service = new DataProcessorService("Прирост населения.csv", "jdbc:sqlite:test.db");
         service.runService();
     }
 }
--- src/main/java/processor/DataProcessorService.java
+++ src/main/java/processor/DataProcessorService.java
@@ -1,19 +1,79 @@
 package processor;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import Database.ConnectionAPI;
+import Database.ConnectionFactory;
+
+import joinery.DataFrame;
+
+/**
+ * В данном модуле реализуется класс с основной бизнес-логикой приложения.
+ * Обычно такие модули / классы имеют в названии слово "Service".
+ */
+
 public class DataProcessorService {
 
     private final DataProcessorFactory procFactory;
 
     private final String datasource;
 
-    public DataProcessorService(String datasource) {
+    private final String dbConnectionUrl;
+
+    public DataProcessorService(String datasource, String dbConnectionUrl) {
         this.datasource = datasource;
+        this.dbConnectionUrl = dbConnectionUrl;
         procFactory = new DataProcessorFactory();
     }
+
+    /**
+     * ВАЖНО! Обратите внимание, что метод runService использует только методы базового абстрактного класса DataProcessor
+     *        и, таким образом, будет выполняться для любого типа обработчика данных (CSV или TXT), что позволяет в дальнейшем
+     *        расширять приложение, просто добавляя другие классы обработчиков, которые, например, работают с базой данных или
+     *        сетевым хранилищем файлов (например, FTP-сервером).
+     */
     public void runService() {
         DataProcessor processor = procFactory.getProcessor(datasource);
-        processor.run();
-        processor.printResult();
+        processor.run();    // обработка данных
+        processor.printResult();    // вывод результата
+        saveToDataBase(processor.getGetResult());   // сохранения результата обработки в БД
+    }
+
+    public void saveToDataBase(DataFrame result) {
+        // Работа с БД
+        if (result != null) {
+            // Подключаемся к БД
+            ConnectionFactory conFactory = new ConnectionFactory();
+            Connection dbConnector = conFactory.getConnection(dbConnectionUrl);
+
+            if (dbConnector != null) {
+                try {
+                    // Добавляем информацию о новом обработанном файле
+                    ConnectionAPI.insertIntoSourceFiles(dbConnector, datasource);
+                    // Выводим все обработанные файлы данных
+                    ResultSet sourceFilesResultSet = ConnectionAPI.selectAllFromSourceFiles(dbConnector);
+                    while (sourceFilesResultSet.next()) {
+                        System.out.println("--------------------");
+                        System.out.println(sourceFilesResultSet.getInt("id"));
+                        System.out.println(sourceFilesResultSet.getString("filename"));
+                        System.out.println(sourceFilesResultSet.getDate("processed"));
+                    }
+                    // Добавляем результат обработки в Базу Данных
+                    ConnectionAPI.insertRowsIntoProcessedData(dbConnector, result);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                } finally {
+                    // Закрываем соединение с БД
+                    try {
+                        dbConnector.close();
+                    } catch (SQLException e) {
+                        System.out.println(e.getMessage());
+                    }
+                }
+            }
+        }
     }
 
     public String getDatasource() {
